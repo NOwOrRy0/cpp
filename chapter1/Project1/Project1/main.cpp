b1e05c299eb20114540cf2c8d3152733823c30fe
nonamed97 <rnqhsem1@naver.com>
2024-09-20 13:03:55 +0900

    2주차 완료

--- chapter1/Project1/Project1/main.cpp
+++ chapter1/Project1/Project1/main.cpp
@@ -3,6 +3,16 @@
 int main() {
 	using namespace std;
 
-	cout << "Hello World!" << endl;
+	cout << "출생연도를 입력하세요. : ";
+	int years;
+	cin >> years;
+	cout << "2002 한일월드컵 당시 한국 나이는 " << 2002 - years << "세입니다." << endl;
 	return 0;
+
+#ifndef _M_X64
+	cout << "이 프로그램은 x86프로그램입니다." << endl;
+#else
+	cout << "이 프로그램은 x64프로그램입니다." << endl;
+#endif
+
 }
---
+++ chapter2/Project1/Project1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+void main() {
+  char str1[] = "C++";
+  cout << sizeof(str1) << endl;
+}
